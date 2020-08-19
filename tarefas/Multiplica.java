fde35d1d85385dbb2f1dfed8ce30fad69fc5cd91
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-08-19 13:15:15 +0000

    feat: create tasks methods

---
+++ tarefas/Multiplica.java
@@ -0,0 +1,66 @@
+package tarefas;
+
+public class Multiplica extends Thread {
+    private String nome;
+    private int valorMultiplicar;
+    private int tempo;
+
+    public Multiplica(String name, int valor) {
+        this.nome = name;
+        this.valorMultiplicar = valor;
+        this.start();
+    }
+
+    public void run() {
+        // System.out.println("Aqui Multiplica");
+        System.out.println("Multiplicacao: " + this.nome);
+
+        for (int x = 0; x <= 10; x++) {
+            System.out.println(this.valorMultiplicar + " x " + x + " = " + (this.valorMultiplicar * x));
+           
+           
+            try {
+                this.sleep(tempo);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+      }
+
+      System.out.println("Fim da thread!!!");
+    }
+
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getValorMultiplicar() {
+        return valorMultiplicar;
+    }
+
+    public void setValorMultiplicar(int valorMultiplicar) {
+        this.valorMultiplicar = valorMultiplicar;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
+
+    
+
+    
+
+
+}
