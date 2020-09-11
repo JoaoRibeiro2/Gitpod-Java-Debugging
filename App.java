a4f28b27eb4c6041561503f0cc460daf0d6fa4aa
JoaoRibeiro2 <joaoribeyro1@gmail.com>
2020-09-11 23:30:36 +0000

    create factorial method

--- App.java
+++ App.java
@@ -1,13 +1,23 @@
 import tarefas.Multiplica;
+import tarefas.Fatora;
+
 
 /**
  * App
  */
 public class App {
 
+   
     public static void main(String[] args) {
-        Multiplica m = new Multiplica("10", 10);
-        m.setTempo(1000);
+        // Multiplica m = new Multiplica("10", 10);
+        // m.setTempo(1000);
+
+        Fatora f = new Fatora(5);
+        f.start();
         // m.start();
     }
+
+    
+
+
 }
